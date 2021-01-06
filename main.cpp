41461d73eb5101b2b1fe0080c5b2bfd7d597d57c
Ming45 <64730108+Ming45@users.noreply.github.com>
2021-01-06 23:08:12 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "mymanage.h"
+int main() {
+/*	Teacherwork y;
+	y.getscoretxt();
+	//y.savescoretxt();
+	y.getstudenttxt();
+	//y.savestudenttxt();
+	y.showallstudent();
+
+	//y.addscore();
+	//y.showcoursestudent();
+
+	y.countclass();
+	y.countcourse();
+
+	y.sortcourse();
+	y.countcourse();*/
+/*
+	Manage z;
+	z.getstudenttxt();
+
+	z.addstudent();
+	z.savestudenttxt();
+
+	z.mremovestudent();
+	z.savestudenttxt();*/
+	Studentwork x;
+
+	//	x.getstudenttxt();
+	//	x.savestudenttxt();
+
+	x.getscoretxt();
+
+	x.getstudenttxt();
+	x.getcoursetxt();
+	//	x.savecoursetxt();
+
+	x.sremovecourse();
+	x.showallcourse();
+	//x.choosecourse();
+
+	x.getcoursecredit();
+	x.getstudentscore();
+	//x.sremovecourse();
+	//x.savescoretxt();
+}
