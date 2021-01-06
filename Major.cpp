6c5214accd7f74325b286394f984f8108e1f51da
Ming45 <64730108+Ming45@users.noreply.github.com>
2021-01-06 23:07:17 +0800

    Add files via upload

---
+++ Major.cpp
@@ -0,0 +1,13 @@
+#include"mymanage.h"
+/*
+Major* Major::findmname(char* name) {
+	for (Major* p = head; p->next != end; p = p->next)
+		if (!strcmp(p->next->mname, name))return p;//strcmp函数是string compare(字符串比较)的缩写，用于比较两个字符串并根据比较结果返回整数。
+												  //基本形式为strcmp(str1,str2)，若str1=str2，则返回零；若str1<str2，则返回负数；若str1>str2，则返回正数
+	return NULL;
+}
+Major* Major::findmid(char* Id) {
+	for (Major* p = head; p->next != end; p = p->next)
+		if (!strcmp(p->next->mid, Id))return p;
+	return NULL;
+}*/
